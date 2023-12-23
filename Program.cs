9ad0e32c45f32b1c5e89f0bcee5a4f89d34154fe
Efim <120276090+efimandreev0@users.noreply.github.com>
2023-12-23 15:22:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB__Unpacker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileAttributes attr = File.GetAttributes(args[0]);
+            bool isDir = false;
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                isDir = true;
+            if (!isDir)
+            {
+                Extract(args[0]);
+                return;
+            }
+            else
+            {
+                Rebuild(args[0]);
+                return;
+            }
+        }
+        public static void Extract(string archive)
+        {
+            var reader = new BinaryReader(File.OpenRead(archive));
+            reader.BaseStream.Position += 4;
+            int fileCount = reader.ReadInt32();
+            int[] fileStart = new int[fileCount];
+            int[] fileEnd = new int[fileCount];
+            for (int i = 0; i < fileCount; i++)
+            {
+                fileStart[i] = reader.ReadInt32();
+                fileEnd[i] = reader.ReadInt32();
+            }
+            string inputDirectory = Path.GetFileNameWithoutExtension(archive) + "\\";
+            Directory.CreateDirectory(Path.GetFileNameWithoutExtension(archive));
+            for (int i = 0; i < fileCount; i++)
+            {
+                reader.BaseStream.Position = fileStart[i];
+                byte[] file = reader.ReadBytes(fileEnd[i]);
+                File.WriteAllBytes(inputDirectory + "File" + i + ".bmg", file);
+            }
+        }
+        public static void Rebuild(string input)
+        {
+            string[] files = Directory.GetFiles(input, "*.bmg", SearchOption.TopDirectoryOnly);
+            int[] filePointers = new int[files.Length];
+            int[] fileLength = new int[files.Length];
+            using (BinaryWriter writer = new BinaryWriter(File.Create(input + ".bb")))
+            {
+                writer.Write(Encoding.UTF8.GetBytes("BB"));
+                writer.BaseStream.Position += 2;
+                writer.Write(files.Length);
+                writer.Write(new byte[files.Length * 8]);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    byte[] file = File.ReadAllBytes(files[i]);
+                    fileLength[i] = file.Length;
+                    filePointers[i] = (int)writer.BaseStream.Position;
+                    writer.Write(file);
+                }
+                writer.BaseStream.Position = 0x8;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    writer.Write(filePointers[i]);
+                    writer.Write(fileLength[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("BB__Unpacker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BB__Unpacker")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bb3a6936-facf-4104-aec0-b5bda63f2259")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
